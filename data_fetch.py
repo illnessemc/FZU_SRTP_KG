208ff0b8119b147f44a2c71857635ae1ab0cdefe
illnessemc <3161811743@qq.com>
2025-11-12 23:08:25 +0800

    11.12 数据获取---失败

--- data_fetch.py
+++ data_fetch.py
@@ -5,6 +5,7 @@ import os
 ts.set_token("f8a65c06d4193356d730640695418b9214465586e5d9a79846557774")
 pro = ts.pro_api()
 
+##data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
 df = pro.stock_company(exchange='SZSE', fields='ts_code,chairman,manager,secretary,reg_capital,setup_date,province')
 print(df)
 
