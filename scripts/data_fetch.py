619d4669b73ed7558a4b8b05e14532a5ee2a82b9
illnessemc <3161811743@qq.com>
2025-11-14 01:53:27 +0800

    25.11.14---data_fetch 2

--- scripts/data_fetch.py
+++ scripts/data_fetch.py
@@ -17,7 +17,7 @@ df_basic = pro.stock_basic(fields='ts_code,name,area,industry,market,exchange,li
 
 csv_path = os.path.join(data_dir, 'stock_basic.csv')
 
-df_basic.to_csv(csv_path, index=False)
+df_basic.to_csv(csv_path, index=False,encoding='utf-8')
 
 #### 将所有股票代码保存到codes.csv方便后续调用
 
@@ -30,19 +30,48 @@ allowed_codes = set(df_codes['ts_code'])  ###所有沪深代码
 config_dir = os.path.join(project_root, 'config')
 os.makedirs(config_dir, exist_ok=True)
 
-csv_path = os.path.join(config_dir, 'codes.csv')
-df_codes.to_csv(csv_path, index=False)
+csv_path = os.path.join(config_dir, 'codes.csv',)
+df_codes.to_csv(csv_path, index=False,encoding='utf-8')
 
 #### 上市公司管理层信息
 
-df_basic = pro.stk_managers(fileds='ts_code,name,edu,lev,begin_date,end_date')
-df_basic = df_basic[df_basic['ts_code'].isin(allowed_codes)]
-
-csv_path = os.path.join(data_dir, 'stk_managers.csv')
-df_basic.to_csv(csv_path, index=False)
+# df_basic = pro.stk_managers(fileds='ts_code,name,edu,lev,begin_date,end_date')
+# df_basic = df_basic[df_basic['ts_code'].isin(allowed_codes)]
+#
+# csv_path = os.path.join(data_dir, 'stk_managers.csv',)
+# df_basic.to_csv(csv_path, index=False,encoding='utf-8')
 
 #### 上市公司主营业务及其盈利情况
 
-#### 股票市场每日前十大成交数据
+config_dir = os.path.join(project_root, 'config')
+codes_path = os.path.join(config_dir, 'codes.csv')
+df_codes = pd.read_csv(codes_path, dtype=str)
+code_list = df_codes['ts_code'].dropna().unique().tolist()
 
-basic = pro.hsgt_top10()
+fina_path=os.path.join(data_dir, 'fina_mainbz')
+os.makedirs(fina_path, exist_ok=True)
+
+for item in code_list:
+    try:
+        df_basic=pro.fina_mainbz(ts_code=item)
+        if (len(df_basic)>0):
+            outpath=f"{fina_path}/{item}.csv"
+            df_basic.to_csv(outpath, index=False, encoding='utf-8')
+    except:
+        continue
+
+#### 股票市场每日前十大成交数据
+# hsgt_top10_path=os.path.join(data_dir, 'hsgt_top10')
+# os.makedirs(hsgt_top10_path, exist_ok=True)
+# start_date = "20240101"
+# end_date   = "20250801"
+# trade_cal = pro.trade_cal(exchange='SSE', start_date=start_date, end_date=end_date)
+# trade_dates = trade_cal[trade_cal['is_open'] == 1]['cal_date'].tolist()
+# for item in trade_dates:
+#     try:
+#         df_baisc=pro.hsgt_top10(trade_date=item)
+#         if (len(df_baisc)>0):
+#             outpath=f"{hsgt_top10_path}/{item}_hsgt_top10.csv"
+#             df_baisc.to_csv(outpath, index=False, encoding='utf-8')
+#     except:
+#         continue
